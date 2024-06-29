2f9e9dd73529b5328bed9805f9d817481f96f636
vmanannikov <manhetan@gmail.com>
2024-06-29 09:53:15 +0300

    update DataInitializer, fix check for exist user

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -5,6 +5,7 @@ import hexlet.code.model.Label;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import hexlet.code.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -25,17 +26,23 @@ public class DataInitializer implements ApplicationRunner {
 
     @Autowired
     private LabelRepository labelRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var userData = new UserCreateDTO();
-        userData.setEmail("manhetan@gmail.com");
-        userData.setPassword(passwordEncoder.encode("qwerty"));
-        userData.setFirstName("Vadim");
-        userData.setLastName("Manannikov");
-        userService.create(userData);
+        if (userRepository.findByEmail("manhetan@gmail.com").isEmpty()) {
+            var userData = new UserCreateDTO();
+            userData.setEmail("manhetan@gmail.com");
+            userData.setPassword(passwordEncoder.encode("qwerty"));
+            userData.setFirstName("Vadim");
+            userData.setLastName("Manannikov");
+            userService.create(userData);
+        }
 
         var taskStatuses = new HashMap<String, String>();
 
@@ -54,13 +61,16 @@ public class DataInitializer implements ApplicationRunner {
                 })
                 .forEach(taskStatusRepository::save);
 
-        var l1 = new Label();
-        var l2 = new Label();
+        if (labelRepository.findByName("feature").isEmpty()) {
+            var l1 = new Label();
+            l1.setName("feature");
+            labelRepository.save(l1);
+        }
 
-        l1.setName("feature");
-        l2.setName("bug");
-
-        labelRepository.save(l1);
-        labelRepository.save(l2);
+        if (labelRepository.findByName("bug").isEmpty()) {
+            var l2 = new Label();
+            l2.setName("bug");
+            labelRepository.save(l2);
+        }
     }
 }
